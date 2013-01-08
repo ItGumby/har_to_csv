5f6c240d4b465b53256a0ba5ec3a160d50b671d8
Daisuke Komazaki <d_komazaki@e-savaway.jp>
2013-01-08 17:49:05 +0900

    フォーマットをより意識した作りに変えた

--- har_to_csv.rb
+++ har_to_csv.rb
@@ -2,22 +2,111 @@ require 'json'
 raise Exception, 'you must provide a json file' unless ARGV[0]
 
 SEP = "\t"
+ENTRY_FORMAT = ["request", "response", "timings", "startedDateTime", "time"]
+
+REQUEST_FORMAT = ["method", "url", "httpVersion", "queryString", "headersSize", "bodySize"]
+
+RESPONSE_FORMAT = [ "status",
+                    "statusText",
+                    "httpVersion",
+                    "content",
+                    "redirectURL",
+                    "headersSize",
+                    "bodySize"]
+
+CONTENT_FORMAT = [ "size",
+                   "mimeType",
+                   "compression"]
+
+TIMING_FORMAT = [ "blocked",
+                  "dns",
+                  "connect",
+                  "send",
+                  "wait",
+                  "receive",
+                  "ssl"]
+
+=begin
+entries structure
+Array
+=> {
+     startedDateTime,
+     time,
+     request
+     => {
+          method,
+          url,
+          httpVersion,
+          headers
+          => Array
+            => {},
+          queryString,
+          cookies
+          => Array
+            => {},
+          headersSize,
+          bodySize
+        },
+     response
+     => {
+          status,
+          statusText,
+          httpVersion,
+          headers
+          => Array
+            => {},
+          cookies
+          => Array
+            => {},
+          content
+          => {
+              size,
+              mimeType,
+              compression
+             },
+          redirectURL,
+          headersSize,
+          bodySize
+        },
+     cache
+     => {
+        },
+     timings
+     => {
+          blocked,
+          dns,
+          connect,
+          send,
+          wait,
+          receive,
+          ssl
+        },
+     pageref
+   }
+=end
 
 def header_to_csv(data)
   header = []
   header << ""
-  data.first.each_pair do |ek,ev| # data.first.class => Hash
-    header << ek.to_s and next if ev.class != Hash
-    ev.reject{|k,v| k == 'headers' || k == 'cookies'}.each_pair do |k,v|
-      if v.class != Hash
-        header << k.to_s
-      else
-        v.each_pair do |k2,v2| # content
-          header << k2.to_s
-        end
-      end
-    end
+  h = data.first
+  # request
+  REQUEST_FORMAT.each do |k|
+    header << k.to_s
   end
+  # response
+  RESPONSE_FORMAT.reject{|k| k == 'content'}.each do |k|
+    header << k.to_s
+  end
+  CONTENT_FORMAT.each do |k|
+    header << k.to_s
+  end
+  # timings
+  TIMING_FORMAT.each do |k|
+    header << k.to_s
+  end
+  # other
+  header << "startedDateTime"
+  header << "time"
   puts header.join(SEP)
 end
 
@@ -25,18 +114,29 @@ def data_to_csv(data)
   data.each_with_index do |entry,i| # data.class => Array
     csv = []
     csv << (i+1).to_s
-    entry.each_pair do |ek,ev| # entry.class => Hash
-      csv << ev.to_s and next if ev.class != Hash
-      ev.reject{|k,v| k == 'headers' || k == 'cookies'}.each_pair do |k,v|
-        if v.class != Hash
-          csv << v.to_s
-        else
-          v.each_pair do |k2,v2| # content
-            csv << v2.to_s
-          end
-        end
-      end
+    # request
+    request = entry["request"]
+    REQUEST_FORMAT.each do |k|
+      csv << request[k].to_s
     end
+    # response
+    response = entry["response"]
+    RESPONSE_FORMAT.reject{|k| k == 'content'}.each do |k|
+      csv << response[k].to_s
+    end
+    content = response['content']
+    CONTENT_FORMAT.each do |k|
+      csv << content[k].to_s
+    end
+    # timings
+    timings = entry["timings"]
+    TIMING_FORMAT.each do |k|
+      csv << timings[k].to_s
+    end
+    # other
+    csv << entry["startedDateTime"].to_s
+    csv << entry["time"].to_s
+
     puts csv.join(SEP)
   end
 end
@@ -51,8 +151,12 @@ def output_json_csv(json)
           puts " "*2 + l3k + ":" + l3v.to_s
         end
       elsif l2v.class == Array # pages,entries
-        header_to_csv(l2v)
-        data_to_csv(l2v)
+        if l2k == 'entries'
+          header_to_csv(l2v)
+          data_to_csv(l2v)
+        else
+
+        end
       else
         puts " "*2 + l2k + ":" + l2v.to_s
       end
